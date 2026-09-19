cd1484d891be265b01d7f1c0a5053369ef433201
SebastianAntic <138672978+SebastianAntic@users.noreply.github.com>
2026-09-19 15:45:31 +0530

    Delete client directory

--- client/src/api/spotify.js
+++
@@ -1,5 +0,0 @@
-export async function getProfile() {
-  const res = await fetch('/api/spotify/me', { credentials: 'include' })
-  if (!res.ok) throw new Error('Not authenticated or failed to fetch')
-  return res.json()
-}
